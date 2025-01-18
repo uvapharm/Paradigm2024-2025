b8cf6f7679f0e2d4bd9b09204636054d6ac1784b
SkinnyPenguin <112586940+SkinnyPenguin@users.noreply.github.com>
2025-01-17 21:08:06 -0500

    l

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumField.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumField.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,7 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MecanumField extends OpMode {
 
     DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, outtakeMotorLeft, outtakeMotorRight;
-    Servo arm, bucket, claw;
+    CRServo arm, bucket;
+    Servo claw;
 
 
     // Linear Slide Encoder Target Positions
@@ -20,6 +22,9 @@ public class MecanumField extends OpMode {
     private int targetPosition = LOW_POSITION;
     private boolean isAutomatedVertical = false;
     private boolean isAutomatedHorizontal = false;
+
+    private boolean prevClaw = false;
+    private boolean clawClosed = false;
 
     private final double HOLDING_POWER = 0;  // Adjust as needed
 
@@ -41,13 +46,15 @@ public class MecanumField extends OpMode {
         outtakeMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Servos
-        arm = hardwareMap.get(Servo.class, "arm");
-        bucket = hardwareMap.get(Servo.class, "bucket");
-        bucket = hardwareMap.get(Servo.class, "claw");
+        arm = hardwareMap.get(CRServo.class, "arm");
+        bucket = hardwareMap.get(CRServo.class, "bucket");
+        claw = hardwareMap.get(Servo.class, "claw");
     }
 
     @Override
     public void loop() {
+
+        boolean currClaw = gamepad2.a;
         // Basic mecanum drive logic
         double x = gamepad1.left_stick_x * 0.6; // Strafe
         double y = -gamepad1.left_stick_y * 0.6; // Forward/backward
@@ -74,26 +81,36 @@ public class MecanumField extends OpMode {
         //-----------------------------------------------------------------------------------------
 
         // Servos
-        if (gamepad2.a) {
-            arm.setPosition(0.8);
-        } else {
-            arm.setPosition(0);
-        }
 
 
-        //bucket
+
+        // arm (continuous)
+        arm.setPower(gamepad2.right_stick_y);
+
+
+        //bucket (continuous)
         if (gamepad2.b) {
-            bucket.setPosition(-0.8);
-        } else {
-            bucket.setPosition(-0.5);
+            bucket.setPower(-0.2);
+        } else if (gamepad2.x){
+            bucket.setPower(0.2);
+        }else {
+            bucket.setPower(0);
         }
 
 
-        if (gamepad2.b) {
-            claw.setPosition(0);
-        } else {
-            claw.setPosition(0.5);
+        //claw (NOT continuous)
+        if (currClaw && !prevClaw){
+            if (clawClosed){
+                claw.setPosition(0.5);
+            } else{
+
+                claw.setPosition(0.3);
+            }
+
+            clawClosed = !(clawClosed);
+
         }
+
 
         // Encoder-based Linear Slide Control
         if (gamepad1.a) {
@@ -151,13 +168,15 @@ public class MecanumField extends OpMode {
             }
         }
 
+        prevClaw = currClaw;
+
         telemetry.addData("Target Position", targetPosition);
         telemetry.addData("Slide Position", outtakeMotorLeft.getCurrentPosition());
         telemetry.addData("Automated Vertical", isAutomatedVertical);
         telemetry.addData("Horizontal Position", outtakeMotorRight.getCurrentPosition());
         telemetry.addData("Automated Horizontal", isAutomatedHorizontal);
-        telemetry.addData("arm",arm.getPosition());
-        telemetry.addData("bucket",bucket.getPosition());
+       // telemetry.addData("arm",arm.getPosition());
+        //telemetry.addData("bucket",bucket.getPosition());
         telemetry.addData("wrist",claw.getPosition());
         telemetry.update();
     }
